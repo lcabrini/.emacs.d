53dfcdb1c4499506e2e29b2f823f0eda4a7653cd
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2020-04-16 00:48:33 +0000

    First commit.

---
+++ init.el
@@ -0,0 +1,44 @@
+;; Personal Emacs configuration
+;; Author: Lorenzo Cabrini <lorenzo.cabrini@gmail.com
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; From Quelpa documentation
+(unless (package-installed-p 'quelpa)
+  (with-temp-buffer
+    (url-insert-file-contents "https://github.com/quelpa/quelpa/raw/master/quelpa.el")
+    (eval-buffer)
+    (quelpa-self-upgrade)))
+
+(quelpa
+ '(quelpa-use-package
+   :fetcher git
+   :url "https://github.com/quelpa/quelpa-use-package.git"))
+(require 'quelpa-use-package)
+
+(require 'org)
+(org-babel-load-file (expand-file-name "~/.emacs.d/settings.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (treemacs-magit treemacs-icons-dired treemacs-projectile treemacs projectile color-theme-sanityinc-tomorrow zenburn-theme which-key web-mode quelpa-use-package powerline pass org-pdfview org-emms org-drill org-bullets matrix-client magit emms elpy elfeed-org counsel company-jedi ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
