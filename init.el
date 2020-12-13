8bae2b03c372c9e599a14b37a5d2925a00ab667c
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2020-12-13 09:33:37 +0000

    Ledger mode configuration

--- init.el
+++ init.el
@@ -34,12 +34,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (treemacs-magit treemacs-icons-dired treemacs-projectile treemacs projectile color-theme-sanityinc-tomorrow zenburn-theme which-key web-mode quelpa-use-package powerline pass org-pdfview org-emms org-drill org-bullets matrix-client magit emms elpy elfeed-org counsel company-jedi ace-window)))
+   '(ledger-mode treemacs-magit treemacs-icons-dired treemacs-projectile treemacs projectile color-theme-sanityinc-tomorrow zenburn-theme which-key web-mode quelpa-use-package powerline pass org-pdfview org-emms org-drill org-bullets matrix-client magit emms elpy elfeed-org counsel company-jedi ace-window))
  '(safe-local-variable-values
-   (quote
-    ((engine . django)
-     (pyenv-activate . "~/src/napkoaco/.venv")))))
+   '((engine . django)
+     (pyenv-activate . "~/src/napkoaco/.venv"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
